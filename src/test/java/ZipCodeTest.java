666d0b41cca32b5580878f1ef475e73042df4559
Elizaveta <elizabeth.frisky@gmail.com>
2024-10-27 11:36:13 +0300

    Initial commit

---
+++ src/test/java/ZipCodeTest.java
@@ -0,0 +1,91 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ZipCodeTest {
+
+    @Test
+    public void test() {
+        //тест-пример, делали на занятии
+        /*
+        Открыть браузер
+        Открыть страницу https://www.sharelane.com/cgi-bin/register.py
+        В поле zip code ввести 1111
+        Нажать кнопку continue
+        Проверить появление ошибки
+        Закрыть браузер
+
+        <input type="text" name="zip_code" value="">
+         */
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://www.sharelane.com/cgi-bin/register.py");
+        browser.findElement(By.name("zip_code")).sendKeys("1111");
+        browser.findElement(By.cssSelector("[value = Continue]")).click();
+        String errorMessage = browser.findElement(By.cssSelector("[class = error_message]")).getText();
+        Assert.assertEquals(errorMessage, "Oops, error on page. ZIP code should have 5 digits");
+        browser.quit();
+    }
+
+    @Test
+    public void test1() {
+        /*
+        Ввести ничего
+         */
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://www.sharelane.com/cgi-bin/register.py");
+        browser.findElement(By.name("zip_code")).sendKeys("");
+        browser.findElement(By.cssSelector("[value = Continue]")).click();
+        String errorMessage = browser.findElement(By.cssSelector("[class = error_message]")).getText();
+        Assert.assertEquals(errorMessage, "Oops, error on page. ZIP code should have 5 digits");
+        browser.quit();
+    }
+
+    @Test
+    public void test2() {
+        /*
+        Ввести 7 цифр
+         */
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://www.sharelane.com/cgi-bin/register.py");
+        browser.findElement(By.name("zip_code")).sendKeys("1234567");
+        browser.findElement(By.cssSelector("[value = Continue]")).click();
+        String errorMessage = browser.findElement(By.cssSelector("[class = error_message]")).getText();
+        Assert.assertEquals(errorMessage, "Oops, error on page. ZIP code should have 5 digits");
+        browser.quit();
+    }
+
+    @Test
+    public void test3() {
+        /*
+        Ввести валидное значение: 12345
+         */
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://www.sharelane.com/cgi-bin/register.py");
+        browser.findElement(By.name("zip_code")).sendKeys("12345");
+        browser.findElement(By.cssSelector("[value = Continue]")).click();
+        String currentUrl = browser.getCurrentUrl();
+        Assert.assertTrue(currentUrl.contains("register"));
+        browser.quit();
+    }
+
+    @Test
+    public void test4() {
+        /*
+        Ввести буквы
+         */
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://www.sharelane.com/cgi-bin/register.py");
+        browser.findElement(By.name("zip_code")).sendKeys("qwert");
+        browser.findElement(By.cssSelector("[value = Continue]")).click();
+        String errorMessage = browser.findElement(By.cssSelector("[class = error_message]")).getText();
+        Assert.assertEquals(errorMessage, "Oops, error on page. ZIP code should have 5 digits");
+        browser.quit();
+    }
+}
